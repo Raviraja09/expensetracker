84105a1e31915fd8a9353b98dd3a389cbc2798a5
Raviraja09 <rajavysyaraju09@gmail.com>
2023-03-27 12:39:04 +0530

    task-7

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from './Components/SignUp';
 import Login from './Components/Login'; 
 import Welcome from "./Components/Welcome";
+import ResetPassword from "./Components/ResetPassword";
+import { Signup } from "./Components/Api";
 
 function App() {
   return (
     <Router>
         <Route path="/" element={<SignUp />} />
         <Route path="/Login" element={<Login />} />
+        <Route path="/SignUp" element={<Signup />} />
         <Route path="/welcome" element={<Welcome />} />
+        <Route path="/ResetPassword" element={<ResetPassword />} />
     </Router>
   );
 }
-
 export default App;
 
 
