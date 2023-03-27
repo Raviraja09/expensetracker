9bb5258f97ca70e3b4a43bdddee832b6056894e9
Raviraja09 <rajavysyaraju09@gmail.com>
2023-03-27 14:12:51 +0530

    task-10

--- src/Components/Action.js
+++ src/Components/Action.js
@@ -37,3 +37,28 @@ export const getUserProfile = async (token, mail) => {
       alert(error);
     }
   }
+  export function addExpeses(exp) {
+    return async () => {
+      async function sendRequest() {
+        const url = `https://expensetracker-c23d7-default-rtdb.firebaseio.com/expenses.json`;
+        try {
+          const response = await fetch(url, {
+            method: "PUT",
+            body: JSON.stringify(exp),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+          const data = await response.json();
+          if (!response.ok) {
+            throw data.error.message;
+          }
+        } catch (error) {
+          alert(error);
+          return;
+        }
+      }
+      await sendRequest();
+    };
+  }
+  
