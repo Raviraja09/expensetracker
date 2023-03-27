8294d5080a022623c90e11a947120e7509d59dd6
Raviraja09 <rajavysyaraju09@gmail.com>
2023-03-27 12:38:25 +0530

    reset assword(task-7)

--- src/Components/Api.js
+++ src/Components/Api.js
@@ -39,3 +39,46 @@ export async function Signup(user) {
       return;
     }
   }
+  export async function ResetPassword(details) {
+    const url =
+      "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyA6Bh9-tRa9uPURWKUS7JJc9T04h1Dph3M";
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(details),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      if (!response.ok) {
+        throw data.error.message;
+      }
+    } catch (error) {
+      alert(error);
+      return;
+    }
+  }
+  export async function verifyEmail(token) {
+    const url =
+      "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyA6Bh9-tRa9uPURWKUS7JJc9T04h1Dph3M";
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+          requestType: "VERIFY_EMAIL",
+          idToken: token,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      if (!response.ok) {
+        throw data.error.message;
+      }
+    } catch (error) {
+      alert(error);
+      return;
+    }
+  }
